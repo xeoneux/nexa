57b15cea79909c4d10d5657de339a749a8b10e3d
Aayush Kapoor <xeoneux@icloud.com>
2017-12-12 15:29:45 +0530

    Configure TSLint

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,8 @@
-import "reflect-metadata"
 import { NestFactory } from "@nestjs/core";
 
 import { ApplicationModule } from "./app.module";
 
 (async () => {
   const app = await NestFactory.create(ApplicationModule);
-  await app.listen(1234);
+  await app.listen(1234); 
 })();
