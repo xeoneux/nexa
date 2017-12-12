66bf6eede3bde137d0bdc486826275e28b16422e
Aayush Kapoor <xeoneux@icloud.com>
2017-12-12 16:33:47 +0530

    Configure Swagger

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,19 @@
 import {NestFactory} from '@nestjs/core';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 
 import {ApplicationModule} from './modules/app.module';
 
 (async () => {
   const app = await NestFactory.create(ApplicationModule);
+
+  const options = new DocumentBuilder()
+                      .setTitle('Nexa')
+                      .setDescription('Nexa API description')
+                      .setVersion('1.0')
+                      .addTag('nexa')
+                      .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/api', app, document);
+
   await app.listen(1234);
 })();
