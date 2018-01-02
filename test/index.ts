d23c4e01b7da5af650cfe19845a188c0e45981b1
Aayush Kapoor <xeoneux@icloud.com>
2018-01-02 16:08:24 +0530

    Use default test express server

--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,6 @@
 import {HttpStatus, INestApplication} from '@nestjs/common';
 import {ValidationPipe} from '@nestjs/common';
 import {Test} from '@nestjs/testing';
-import * as express from 'express';
 
 import {ApplicationModule} from '../src/modules/app.module';
 
@@ -11,7 +10,7 @@ export const getNestApplication = async () => {
   if (app) return app;
   const module =
       await Test.createTestingModule({modules: [ApplicationModule]}).compile();
-  app = module.createNestApplication(express());
+  app = module.createNestApplication();
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('v1');
   return app;
